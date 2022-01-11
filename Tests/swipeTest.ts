38a3df86b76a2b17b8b8c16d8578f9b4b54755de
Valentina Schwan <56428408+vschwan@users.noreply.github.com>
2022-01-11 15:12:28 +0100

    add testSwipe

---
+++ Tests/swipeTest.ts
@@ -0,0 +1,54 @@
+var container:Element = <Element> document.querySelector("CLASS OR ID FOR WHERE YOU WANT TO DETECT SWIPE");
+
+  container.addEventListener("touchstart", startTouch, false);
+  container.addEventListener("touchmove", moveTouch, false);
+
+  // Swipe Up / Down / Left / Right
+  var initialX: any = null;
+  var initialY:any = null;
+
+  function startTouch(e:any) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+  };
+
+  function moveTouch(e:any) {
+    if (initialX === null) {
+      return;
+    }
+
+    if (initialY === null) {
+      return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      // sliding horizontally
+      if (diffX > 0) {
+        // swiped left
+        console.log("swiped left");
+      } else {
+        // swiped right
+        console.log("swiped right");
+      }  
+    } else {
+      // sliding vertically
+      if (diffY > 0) {
+        // swiped up
+        console.log("swiped up");
+      } else {
+        // swiped down
+        console.log("swiped down");
+      }  
+    }
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+  };
