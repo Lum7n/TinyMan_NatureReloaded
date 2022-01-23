f553ea0d1fa1d8a7aeb2ba33e01206f2bb99d6ae
Lum7n <56406099+Lum7n@users.noreply.github.com>
2022-01-23 20:07:24 +0100

    script added

--- Tag der Medien/script.js
+++ Tag der Medien/script.js
@@ -4,6 +4,18 @@ var Tag_der_Medien;
     window.addEventListener("load", handleLoad);
     function handleLoad() {
         console.log("script");
+        backgroundSize();
+    }
+    function backgroundSize() {
+        let mainContent = document.querySelector(".main");
+        let heightContent = mainContent.offsetHeight;
+        heightContent = heightContent + 180;
+        console.log(heightContent);
+        let css = ".block_1:after{ height: " + heightContent + "px }";
+        console.log(css);
+        let style = document.createElement("style");
+        style.appendChild(document.createTextNode(css));
+        document.getElementsByTagName("head")[0].appendChild(style);
     }
 })(Tag_der_Medien || (Tag_der_Medien = {}));
 //# sourceMappingURL=script.js.map
--- Tag der Medien/script.ts
+++ Tag der Medien/script.ts
@@ -2,9 +2,28 @@ namespace Tag_der_Medien {
 
     window.addEventListener("load", handleLoad);
 
-
     function handleLoad(): void {
     
         console.log("script");
+        backgroundSize();
+
+    }
+
+    function backgroundSize(): void {
+
+        let mainContent: HTMLElement = <HTMLElement>document.querySelector(".main");
+        let heightContent: number = mainContent.offsetHeight;
+
+        heightContent = heightContent + 180;
+        console.log(heightContent);
+
+        let css: string = ".block_1:after{ height: " + heightContent + "px }";
+        console.log(css);
+
+        let style: HTMLStyleElement = <HTMLStyleElement>document.createElement("style");
+
+        style.appendChild(document.createTextNode(css));
+
+        document.getElementsByTagName("head")[0].appendChild(style);
     }
 }
