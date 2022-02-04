0c15b1334ce9f69fc632e4db0d81467af4f7b1fc
Lum7n <56406099+Lum7n@users.noreply.github.com>
2022-02-04 23:44:58 +0100

    Micropage three sides

--- Micropage/script.js
+++ Micropage/script.js
@@ -2,9 +2,18 @@
 var Tag_der_Medien;
 (function (Tag_der_Medien) {
     window.addEventListener("load", handleLoad);
+    let ideaButton;
+    let conceptButton;
+    let prototypButton;
     function handleLoad() {
         console.log("script");
         backgroundSize();
+        ideaButton = document.getElementById("ideaButton");
+        ideaButton.addEventListener("click", handleIdea);
+        conceptButton = document.getElementById("conceptButton");
+        conceptButton.addEventListener("click", handleConcept);
+        prototypButton = document.getElementById("prototypButton");
+        prototypButton.addEventListener("click", handlePrototyp);
     }
     function backgroundSize() {
         let mainContent = document.querySelector(".main");
@@ -16,6 +25,33 @@ var Tag_der_Medien;
         let style = document.createElement("style");
         style.appendChild(document.createTextNode(css));
         document.getElementsByTagName("head")[0].appendChild(style);
+    }
+    function handleIdea() {
+        console.log("Idee");
+        let ideaDiv = document.querySelector(".idea");
+        ideaDiv.style.display = "block";
+        let conceptDiv = document.querySelector(".concept");
+        conceptDiv.style.display = "none";
+        let prototypDiv = document.querySelector(".prototyp");
+        prototypDiv.style.display = "none";
+    }
+    function handleConcept() {
+        console.log("Konzeption");
+        let ideaDiv = document.querySelector(".idea");
+        ideaDiv.style.display = "none";
+        let conceptDiv = document.querySelector(".concept");
+        conceptDiv.style.display = "block";
+        let prototypDiv = document.querySelector(".prototyp");
+        prototypDiv.style.display = "none";
+    }
+    function handlePrototyp() {
+        console.log("Prototyp");
+        let ideaDiv = document.querySelector(".idea");
+        ideaDiv.style.display = "none";
+        let conceptDiv = document.querySelector(".concept");
+        conceptDiv.style.display = "none";
+        let prototypDiv = document.querySelector(".prototyp");
+        prototypDiv.style.display = "block";
     }
 })(Tag_der_Medien || (Tag_der_Medien = {}));
 //# sourceMappingURL=script.js.map
--- Micropage/script.ts
+++ Micropage/script.ts
@@ -2,11 +2,23 @@ namespace Tag_der_Medien {
 
     window.addEventListener("load", handleLoad);
 
+    let ideaButton: HTMLButtonElement;
+    let conceptButton: HTMLButtonElement;
+    let prototypButton: HTMLButtonElement;
+
     function handleLoad(): void {
     
         console.log("script");
         backgroundSize();
 
+        ideaButton = <HTMLButtonElement>document.getElementById("ideaButton");
+        ideaButton.addEventListener("click", handleIdea);
+
+        conceptButton = <HTMLButtonElement>document.getElementById("conceptButton");
+        conceptButton.addEventListener("click", handleConcept);
+
+        prototypButton = <HTMLButtonElement>document.getElementById("prototypButton");
+        prototypButton.addEventListener("click", handlePrototyp);
     }
 
     function backgroundSize(): void {
@@ -25,5 +37,44 @@ namespace Tag_der_Medien {
         style.appendChild(document.createTextNode(css));
 
         document.getElementsByTagName("head")[0].appendChild(style);
+    }
+
+    function handleIdea(): void {
+        console.log("Idee");
+
+        let ideaDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".idea");
+        ideaDiv.style.display = "block";
+
+        let conceptDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".concept");
+        conceptDiv.style.display = "none";
+
+        let prototypDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".prototyp");
+        prototypDiv.style.display = "none";
+    }
+
+    function handleConcept(): void {
+        console.log("Konzeption");
+
+        let ideaDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".idea");
+        ideaDiv.style.display = "none";
+
+        let conceptDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".concept");
+        conceptDiv.style.display = "block";
+
+        let prototypDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".prototyp");
+        prototypDiv.style.display = "none";
+    }
+
+    function handlePrototyp(): void {
+        console.log("Prototyp");
+
+        let ideaDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".idea");
+        ideaDiv.style.display = "none";
+
+        let conceptDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".concept");
+        conceptDiv.style.display = "none";
+
+        let prototypDiv: HTMLDivElement = <HTMLDivElement>document.querySelector(".prototyp");
+        prototypDiv.style.display = "block";
     }
 }
